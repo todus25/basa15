60b8528a946951dd08fa9c1e200102a46a339388
todus25 <almostfemous@gmail.com>
2016-10-22 13:14:06 +0600

    aro asbe

---
+++ custom.js
@@ -0,0 +1,42 @@
+  
+	/*var x=2;
+	 if(x==2){document.getElementById('a').innerHTML='this is r8';}
+	 else if(x==3){document.getElementById('a').innerHTML='this is in middle';}
+	 else{document.getElementById('a').innerHTML='this is wrong';}*?
+	 
+	 /*  var x=3;
+	 if(x==2){document.getElementsByClass('a')[0].innerHTML='this is r8';}
+	 else if(x==3){document.getElementsByClassName('a')[0].innerHTML='this is in middle';}
+	 else{document.getElementsByClassName('a')[0].innerHTML='this is wrong';}
+	*/
+	 /* var x=3;
+	 if(x==2){document.getElementsByTagName('h2')[0].innerHTML='this is r8';}
+	 else if(x==3){document.getElementsByTagName('h2')[0].innerHTML='this is in middle';}
+	 else{document.getElementsByTagName('h2')[0].innerHTML='this is wrong';}
+	*/
+	
+	
+	/*var x=1;
+	
+	switch(x){
+		case 1:
+		document.getElementsByTagName('div')[0].innerHTML='Hello';
+		document.getElementsByTagName('div')[0].style.background='blue';
+		break;
+		default:
+		document.getElementsByTagName('h2')[0].innerHTML='this is wrong';
+		break;
+		}*/
+		//sob css trics javascript e kora jay
+		document.getElementById('b').innerHTML='Hello';
+		document.getElementById('b').style.background='green';
+		document.getElementById('b').style.textAlign='center';
+		
+		
+       document.getElementsByTagName('div')[0].innerHTML='Hello';
+		document.getElementsByTagName('div')[0].style.background='red'; 
+		
+		document.getElementsByClassName('a')[0].innerHTML='Hello';
+		document.getElementsByClassName('a')[0].style.background='blue';
+		
+		
